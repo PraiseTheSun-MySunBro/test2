53171a5e0cde1b902ad2db4de5b26fc54b04e01e
Chapaev <erpris@ttu.ee>
2018-03-18 18:11:07 +0200

    test

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     private static final String msg = "Hello World!";
+    private static final String value = "Wat?";
 
     public static void main (String[] args) {
         System.out.println(msg);
